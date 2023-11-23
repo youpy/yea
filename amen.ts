84f6ce492268410ed2b59a01352b0f82933accb9
youpy <9128+youpy@users.noreply.github.com>
2023-11-24 08:00:37 +0900

    add `Step.duration`

--- amen.ts
+++ amen.ts
@@ -1,18 +1,31 @@
 import asset from './asset.ts'
 
+type ArrayOneOrMore<T> = {
+  0: T
+} & Array<T>
+
 type Sequence = Step[]
 
-type Value = {
-  type: 'rand'
-  value: [Value, Value]
-} | {
-  type: 'pick'
-  value: Value[]
-} | number
+type Value =
+  | {
+    type: 'rand'
+    value: [Value, Value]
+  }
+  | {
+    type: 'pick'
+    value: ArrayOneOrMore<Value>
+  }
+  | {
+    type: 'seq'
+    value: ArrayOneOrMore<Value>
+  }
+  | number
 
 interface Step {
   sliceIndex: Value
-  length: Value // 0.0 - 1.0
+  length?: Value // 0.0 - 1.0
+  duration?: Value // 0.0 - 1.0
+  start?: Value // 0.0 - 1.0
   loop?: {
     prob: Value // 0.0 - 1.0
     length?: Value // 0.0 - 1.0
@@ -35,6 +48,13 @@ const value = (v: Value): number => {
   if (v.type === 'rand') {
     return value(v.value[0]) +
       Math.random() * (value(v.value[1]) - value(v.value[0]))
+  } else if (v.type === 'seq') {
+    const val = v.value[0]
+
+    v.value.shift()
+    v.value.push(val)
+
+    return value(val)
   }
 
   return value(v.value[Math.floor(Math.random() * v.value.length)])
@@ -74,12 +94,20 @@ export const amen = (
       console.log(ac.currentTime)
 
       let index = 0
+      let duration = 0
 
       for (let i = 0; i < sequence.length; i++) {
         const step = sequence[stepIndex(i, sequence[i])]
+        const stepDuration = sliceDuration *
+          (step.duration ? value(step.duration) : 1.0)
         const source = ac.createBufferSource()
-        const stepLength = value(step.length)
+        const stepLength = stepDuration *
+          (step.length ? value(step.length) : 1.0)
         const stepSliceIndex = value(step.sliceIndex)
+        const stepStart = stepDuration * (step.start ? value(step.start) : 1.0)
+        const stepWhen = currentTime + duration
+        const offset = ((sliceDuration * stepSliceIndex) + stepStart) %
+          audioBuffer.duration
 
         if (step.hop) {
           if (Math.random() < value(step.hop.prob)) {
@@ -91,11 +119,12 @@ export const amen = (
 
         if (stepLength > 0) {
           source.buffer = audioBuffer
+          source.loop = true
           source.connect(gain)
           source.start(
-            currentTime + index * sliceDuration,
-            (stepSliceIndex % numSlices) * sliceDuration,
-            sliceDuration * stepLength,
+            stepWhen,
+            offset,
+            stepLength,
           )
         }
 
@@ -104,32 +133,32 @@ export const amen = (
           step.loop && stepLength < 1.0 && step.loop.length &&
           Math.random() < value(step.loop.prob)
         ) {
-          const stepLoopLength = value(step.loop.length)
+          const stepLoopLength = stepDuration * value(step.loop.length)
           const source = ac.createBufferSource()
+          const loopStart = (offset + stepLength) % audioBuffer.duration
+          const loopEnd = loopStart + stepLoopLength
 
           source.buffer = audioBuffer
           source.loop = true
-          source.loopStart = (stepSliceIndex % numSlices) * sliceDuration +
-            (sliceDuration * stepLength)
-          source.loopEnd = source.loopStart + sliceDuration * stepLoopLength
+          source.loopStart = loopStart
+          source.loopEnd = loopEnd
           source.connect(gain)
           source.start(
-            currentTime + index * sliceDuration +
-              (sliceDuration * stepLength),
-            (stepSliceIndex % numSlices) * sliceDuration +
-              (sliceDuration * stepLength),
-            sliceDuration * (1.0 - stepLength),
+            stepWhen + stepLength,
+            loopStart,
+            stepDuration - stepLength,
           )
         }
 
         index++
+        duration += stepDuration
       }
 
-      currentTime += sliceDuration * index
+      currentTime += duration
 
       timeoutId = setTimeout(
         schedule,
-        sliceDuration * index * 0.90 * 1000,
+        duration * 0.90 * 1000,
       )
     }
 
